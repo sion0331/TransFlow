9b65ec6e71d540c0ddb11b80ca879456610c80d4
Sion Chun <sc3791@columbia.edu>
2025-05-04 04:24:22 +0000

    update results

--- utils/loader.py
+++ utils/loader.py
@@ -2,8 +2,10 @@ import torch
 from torch.utils.data import Dataset
 
 class LOBDataset(Dataset):
-    def __init__(self, X, y):
-        self.X = torch.tensor(X, dtype=torch.float32)
+    def __init__(self, X, y, mode=False):
+        self.X = torch.tensor(X, dtype=torch.float32) # (B, 100, 40) for transLOB
+        if mode:
+            self.X= self.X.unsqueeze(1) # (B, 1, 100, 40) for deepLOB
         self.y = torch.tensor(y, dtype=torch.long)
 
     def __len__(self):
